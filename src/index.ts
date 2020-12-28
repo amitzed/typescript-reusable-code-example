6eb336e93b417eae8c53511b6660e27a98bf2035
amitzed <amitzmn@gmail.com>
2020-12-28 17:52:17 -0500

    csv data showing on console log

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+})
+.split('/n')
+.map((row: string): string[] => {
+  return row.split(',')
+})
+console.log(matches);
